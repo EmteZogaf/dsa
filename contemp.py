677af01fe5ec4d3b5c9b4a9d41eb758d3f15a3e1
Mathias Rühle <mathias@life-511480.life.uni-leipzig.local>
2013-05-17 12:50:25 +0200

    [ADD] first commit of kernel [ADD] sequential version for comparison speedup

--- contemp.py
+++ contemp.py
@@ -1,7 +1,27 @@
-import pyopencl as cl, numpy as ny
+# coding: utf8
+import pyopencl as cl, numpy as np, os, time
 
-ctx = cl.create_some_context()
-fil = open('dna.seq')
+class ListCounter(dict):
+	def __missing__(self, key):
+		self[key] = []
+		return self[key]
 
-if not fil:
-	exit(1)
+fn = 'dna.seq'
+f = open(fn)
+f_size = os.path.getsize(fn)
+counts = ListCounter()
+pattern_size_min = 2
+pattern_size_max = 2
+start = time.time()
+
+for i in range(pattern_size_min, pattern_size_max + 1):
+	f.seek(0)
+	a = f.read(i)
+	counts[a].append(0)
+
+	for j in range(1, f_size - i + 1):
+		a = a[1:] + f.read(1)
+		counts[a].append(j)
+end = time.time()
+print(len(counts))
+print('Elapsed time: {0}µs.'.format(int((end-start)*10**6)))
---
+++ pattern_search.cl
@@ -0,0 +1,1 @@
+__kernel void pattern_search(__global const uchar *txt,
